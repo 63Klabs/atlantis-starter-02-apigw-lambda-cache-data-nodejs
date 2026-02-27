6420ea922838b8d0a11980f5f92382a5a4a353d2
ChadKluck <chadkluck@gmail.com>
2026-02-26 18:05:36 -0600

    updated documentation in comments

--- application-infrastructure/src/index.js
+++ application-infrastructure/src/index.js
@@ -1,45 +1,50 @@
-/* 
-*******************************************************************************
-Web Service API
-*******************************************************************************
-
-Author: YOU
-
--------------------------------------------------------------------------------
-
-This utilizes an application starter template for an AWS Lambda function that 
-provides an api service via API Gateway. 
-Internal caching utilizes DynamoDb and S3 through the
-npm package @63klabs/cache-data
-
-Application Starter Template:
-https://github.com/63klabs/atlantis-starter-02-apigw-lambda-cache-data-nodejs
-
--------------------------------------------------------------------------------
-
-For other notes and info, refer to README.md
-
-*******************************************************************************
+/**
+ * Lambda handler
+ * 
+ * The handler performs cold start initialization via Config.init() and delegates
+ * request routing to the Routes module.
+ * 
+ * Uncaught errors are handled within try/catch/finally to provide logs and
+ * error messages back to the client.
+ * 
+ * @module handler
 */
 
-"use strict";
-
+// >! Web service and cache framework package
 const { tools: {DebugAndLog, Response, Timer} } = require("@63klabs/cache-data");
 
+// >! Application Modules
 const { Config } = require("./config");
 const Routes = require("./routes");
 
-/* log a cold start and keep track of init time */
+// >! Log a cold start and time it using Timer - stop Timer in finally block
 const coldStartInitTimer = new Timer("coldStartTimer", true);
 
-/* initialize the Config */
-Config.init(); // we will await completion in the handler
+// >! Initialize Config - done outside of handler so it is only done on cold start
+Config.init(); // >! we will await completion in the handler
 
 /**
  * Lambda function handler
  * 
- * @param {object} event Lambda event - doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {object} context Lambda context - doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * This function is invoked by API Gateway for all incoming requests.
+ * The handler will ensure the cold start init was resolved (either during
+ * the current or prior invocation) and then sends the request to
+ * Routes.process()
+ * 
+ * @async
+ * @param {Object} event - API Gateway event object
+ * @param {Object} event.body - Request body (JSON string)
+ * @param {Object} event.headers - Request headers
+ * @param {Object} event.queryStringParameters - Query parameters
+ * @param {Object} event.requestContext - Request context with requestId, IP, etc.
+ * @param {Object} context - Lambda context object
+ * @param {string} context.requestId - Lambda request ID
+ * @param {string} context.functionName - Lambda function name
+ * @param {number} context.getRemainingTimeInMillis - Function to get remaining execution time
+ * @returns {Promise<Object>} API Gateway response object
+ * @returns {number} returns.statusCode - HTTP status code
+ * @returns {Object} returns.headers - Response headers
+ * @returns {string} returns.body - Response body (JSON string)
  */
 exports.handler = async (event, context) => {
 
@@ -47,13 +52,18 @@ exports.handler = async (event, context) => {
 
 	try {
 
-		/* wait for Config.promise and THEN Config.prime to be settled as we need it before continuing. */
-		await Config.promise(); // makes sure general config init is complete
-		await Config.prime(); // makes sure all prime tasks (tasks that need to be completed AFTER init but BEFORE handler) are completed
-		/* If the cold start init timer is running, stop it and log. This won't run again until next cold start */
+		// >! Ensure Cold Start init is done and primed (all async complete) before continuing.
+		// >! If this is a cold start, we may need to wait
+		// >! If this is not a cold start then all promises will have already been resolved previously and we will move on
+		await Config.promise(); // >! makes sure general config init is complete
+		await Config.prime(); // >! makes sure all prime tasks (tasks that need to be completed AFTER init but BEFORE handler) are completed
+		// >! If the cold start init timer is running, stop it and log. This won't run again until next cold start
 		if (coldStartInitTimer.isRunning()) { DebugAndLog.log(coldStartInitTimer.stop(),"COLDSTART"); }
+		// >! Now that we have verified that all cold start tasks have completed we can continue handling the request
 
-		/* Process the request and wait for result */
+
+		// >! Delegate request processing to routing layer
+		// >! Routes.process() handles tool routing and controller invocation
 		response = await Routes.process(event, context);
 
 	} catch (error) {
@@ -69,7 +79,7 @@ exports.handler = async (event, context) => {
 
 	} finally {
 		DebugAndLog.debug("Response from Handler: ", response);
-		/* Send the result back to API Gateway */
+		// >! Send the result back to API Gateway (finalize will log the request and response status)
 		return response.finalize();
 	}
 
